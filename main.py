e32d025464004021c0be0fe625655b5bbbc785a5
Istiak Hassan Emon <emon5122@gmail.com>
2022-08-06 02:25:02 +0600

    Loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from faker import Faker
 
 faker = Faker()
 i=0
-while i<=10:
+j=int(input("How many fake profiles do you want?\n"))
+while i<=j:
     print(faker.simple_profile(sex=None))
     i=i+1
